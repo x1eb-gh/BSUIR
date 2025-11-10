3db424a63664572051c00b39be22d8a38c5943a9
x1eb <108519394+x1eb-gh@users.noreply.github.com>
2025-11-10 20:00:43 +0300

    laba1 CPP

--- CPP/laba1/laba1.cpp
+++ CPP/laba1/laba1.cpp
@@ -15,19 +15,16 @@ void main()
     double x, y, z;
 
     puts("Hi! Please, print values of variables!");
-    cout << "x =  ";
-    cin >> x;
-    cout << "y =  ";
-    cin >> y;
-    printf("z =  ");
-    scanf_s("%lf", &z);
+    printf("x =  "); scanf_s("%lf", &x);
+    printf("y =  "); scanf_s("%lf", &y);
+    printf("z =  "); scanf_s("%lf", &z);
 
-    double res_1 = pow(y, -(sqrt(abs(x))));
+    double res_1 = pow(y, -(sqrt(fabs(x))));
     double res_2 = (log(res_1)) * (x - y / 2);
     double res_3 = pow(sin(atan(z)), 2);
 
     double result = res_2 + res_3;
-    cout << "Result = " << result << endl;
+    printf("Result = %lf\n", result);
     printf("Press any key to exit.... ");
     _getch();
 }
