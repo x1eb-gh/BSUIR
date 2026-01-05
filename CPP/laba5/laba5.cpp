e70dfb66a447932fa4520a1275e44f5d29e40bac
x1eb <108519394+x1eb-gh@users.noreply.github.com>
2026-01-05 21:42:25 +0300

    laba5

--- CPP/laba5/laba5.cpp
+++ CPP/laba5/laba5.cpp
@@ -1,7 +1,4 @@
-﻿// laba5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-
+﻿
 #include <iostream>
 #include <conio.h>
 #include <stdlib.h>
@@ -9,31 +6,36 @@
 
 using namespace std;
 
-void main() {
+int main() {
+
     int n = 0;
     srand(time(0));
+
     do {
         printf("n <= 20 = "); scanf_s("%i", &n);
-        if (n < 1 || n > 20)
+        if (n < 1 || n > 20) {
             puts("ERROR! Wrong N!");
+        }
     } while (n < 1 || n > 20);
+
     // 5.	Произведение элементов массива, расположенных между первым и вторым нулевыми элементами.
     int a[20] = {};
-    int sw = 0;
+    int sw;
     int furst_null_index = -1 , second_null_index = -1;
-    puts("0 - random variables (default), 1 - from keyboard");
+    puts("1 - from keyboard, other - random variables");
     scanf_s("%i", &sw);
+
     for (int i = 0; i < n; i++) {
+
         switch (sw) {
-            case 1: {
+            case 1:
                 scanf_s("%i", &a[i]);
                 break;
-            }
-            default: {
-                a[i] = rand() % 100 + 10;
+            default:
+                a[i] = rand() % 10;
                 break;
-            }
         }
+
         if (a[i] == 0) {
             if (furst_null_index == -1) {
                 furst_null_index = i;
@@ -43,6 +45,7 @@ void main() {
             }
         }
     }
+
     if (furst_null_index == -1 || second_null_index == -1) {
         puts("Not enough zero elements found!");
     }
@@ -51,15 +54,6 @@ void main() {
         for (int i = furst_null_index + 1; i < second_null_index; i++) proizv *= a[i];
         printf("%i", proizv);
     }
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
