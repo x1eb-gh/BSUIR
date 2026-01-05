10c5c3f305937e76ecbc39912840dac3a725afa1
x1eb <108519394+x1eb-gh@users.noreply.github.com>
2026-01-05 20:17:17 +0300

    laba2

--- CPP/laba2/laba2.cpp
+++ CPP/laba2/laba2.cpp
@@ -1,20 +1,21 @@
-﻿// laba2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-void main() {
-    double x, z, f = 0., a, b;
+double main() {
+    double x, z, f, a, b;
     int select;
 
     f = 0.;
 
-    printf("\nHi! Print values of variables : \na =  "); scanf_s("%lf", &a);
-    printf("b =  "); scanf_s("%lf", &b);
-    printf("z =  "); scanf_s("%lf", &z);
+    printf("\nHi! Print values of variables : \na =  ");
+    scanf_s("%lf", &a);
+    printf("b =  ");
+    scanf_s("%lf", &b);
+    printf("z =  ");
+    scanf_s("%lf", &z);
    
     if (z < -1) {
         printf("\nYou choose z = %lf , your x = -z / 3\n", z);
@@ -30,10 +31,21 @@ void main() {
         printf("Please, select function:\n1:\t2x\n2:\tx^2\n3:\tx/3\n\n");
         scanf_s("%i", &select);
         switch (select) {
-            case 1: f = 2 * x; printf("\nYou select: f = 2x"); break;
-            case 2: f = pow(x, 2); cout << "\nYou select f = x^2" << endl; break;
-            case 3: f = (x * 1.) / 3.; cout << "\nYou select f = x/3" << endl; break;
-            default: cout << "\nERROR!!! Uncnown number!!!\n" << endl; break;
+            case 1: 
+                f = 2 * x; 
+                printf("\nYou select: f = 2x"); 
+                break;
+            case 2: 
+                f = pow(x, 2);
+                printf("\nYou select f = x^2"); 
+                break;
+            case 3: 
+                f = (x * 1.) / 3.; 
+                printf("\nYou select f = x / 3"); 
+                break;
+            default: 
+                printf("\nERROR!!! Uncnown number!!!\n"); 
+                break;
         }
     } while ((select > 3) || (select < 1));
 
@@ -42,15 +54,5 @@ void main() {
     res_2 = log(x + 2.5);
     res_3 = 2 * b * sinh(x);
     printf("\ny = %lf", (res_1 - res_2 + res_3));
+    return res_1 - res_2 + res_3;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
